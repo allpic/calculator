7eeb90c7e9be826046c6beee017e2aef8d43e19e
Madmaxx <madmaxx@L340.fast-reload.com>
2022-11-03 18:17:30 +0100

    Add sum logic, controller and unit test

--- src/main/java/com/leszko/calculator/CalculatorApplication.java
+++ src/main/java/com/leszko/calculator/CalculatorApplication.java
@@ -11,11 +11,3 @@ public class CalculatorApplication {
 	}
 
 }
-package com.leszko.calculator;
-import org.springframework.stereotype.Service;
-@Service
-public class Calculator {
-public int sum(int a, int b) {
-return a + b;
-}
-}
