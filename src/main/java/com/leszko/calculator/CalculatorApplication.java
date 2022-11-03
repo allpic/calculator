4061c4c825f745c20f36cc7a5d11dcc14711e5f2
Madmaxx <madmaxx@L340.fast-reload.com>
2022-11-03 18:06:02 +0100

    add class

--- src/main/java/com/leszko/calculator/CalculatorApplication.java
+++ src/main/java/com/leszko/calculator/CalculatorApplication.java
@@ -11,3 +11,11 @@ public class CalculatorApplication {
 	}
 
 }
+package com.leszko.calculator;
+import org.springframework.stereotype.Service;
+@Service
+public class Calculator {
+public int sum(int a, int b) {
+return a + b;
+}
+}
